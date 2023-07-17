821d36ece4a3b9af5f4c8f4944ced2e0a0f16c96
Atulkumar Manchalwar <117819775+DrAtulkumar@users.noreply.github.com>
2023-07-17 16:05:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 //complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+}
+
+// Student subclass extending Person
+class Student extends Person {
+  // Constructor is inherited from the Person class
+
+  // Method specific to Student class
+  study() {
+    console.log(`${this.name} is studying.`);
+  }
+}
+
+// Teacher subclass extending Person
+class Teacher extends Person {
+  // Constructor is inherited from the Person class
+
+  // Method specific to Teacher class
+  teach() {
+    console.log(`${this.name} is teaching.`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
